25f0824439d9dec9d730546a8a9de0f6475b25c8
svi-anon <ep.ruaro@protonmail.com>
2026-09-16 13:27:07 -0300

    Roles e users de teste

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,32 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@osrs.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'Moderator',
+            'email' => 'moderator@osrs.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'moderator',
+        ]);
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@osrs.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'user',
         ]);
     }
 }
